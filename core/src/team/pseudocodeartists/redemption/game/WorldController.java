b7922e05deb3bcb7c74683033bfa1cd87f518311
Night-Voyager <18722007@bjtu.edu.cn>
2021-05-23 20:00:37 +0800

    Receive and handle input events

--- core/src/team/pseudocodeartists/redemption/game/WorldController.java
+++ core/src/team/pseudocodeartists/redemption/game/WorldController.java
@@ -1,16 +1,41 @@
 package team.pseudocodeartists.redemption.game;
 
-public class WorldController {
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+
+public class WorldController extends InputAdapter {
     public static final String TAG = WorldController.class.getName();
 
     public WorldController() {
+        init();
     }
 
     private void init() {
-
+        Gdx.input.setInputProcessor(this);
     }
 
     public void update(float deltaTime) {
+        handleDesktopInput(deltaTime);
+    }
 
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println(Keys.toString(keycode));
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println(Keys.toString(keycode));
+        return false;
+    }
+
+    private void handleDesktopInput(float deltaTime) {
+        if (Gdx.app.getType() != Application.ApplicationType.Desktop) return;
+
+        if (Gdx.input.isKeyPressed(Keys.A))
+            System.out.println('A');
     }
 }
