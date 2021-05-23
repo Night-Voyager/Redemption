7989ea67375049daed5b108df6f3182d8f2711cb
Night-Voyager <18722007@bjtu.edu.cn>
2021-05-23 17:00:49 +0800

    Initialize the game world

--- core/src/team/pseudocodeartists/redemption/Redemption.java
+++ core/src/team/pseudocodeartists/redemption/Redemption.java
@@ -1,33 +1,64 @@
 package team.pseudocodeartists.redemption;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import team.pseudocodeartists.redemption.game.WorldController;
+import team.pseudocodeartists.redemption.game.WorldRenderer;
 
 public class Redemption extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public static final String TAG = Redemption.class.getName();
+
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+
+	private boolean paused; // for pause or resume event on Android
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		// Set Libgdx log level to DEBUG (LOG_NONE or LOG_INFO are more appropriate before publishing)
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		// Game world is active on start
+		paused = false;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
+		worldRenderer.resize(width, height);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		// Do not update game world when paused
+		if (!paused) {
+			// Update game world by the time that has passed since last rendered frame
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		// Sets the clear screen color to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		// Clear the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Render game world to screen
+		worldRenderer.render();
+	}
+
+	@Override
+	public void pause() {
+		paused = true;
+	}
+
+	@Override
+	public void resume() {
+		paused = false;
+	}
+
+	@Override
+	public void dispose() {
+		worldRenderer.dispose();
 	}
 }
---
+++ core/src/team/pseudocodeartists/redemption/game/WorldController.java
@@ -0,0 +1,16 @@
+package team.pseudocodeartists.redemption.game;
+
+public class WorldController {
+    public static final String TAG = WorldController.class.getName();
+
+    public WorldController() {
+    }
+
+    private void init() {
+
+    }
+
+    public void update(float deltaTime) {
+
+    }
+}
